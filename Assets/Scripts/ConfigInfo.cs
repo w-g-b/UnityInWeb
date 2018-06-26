7aba1971427bc5c9f976bd5b984e3edb73d073b8
GreenLight <1251191746@qq.com>
2018-06-26 19:12:06 +0800

    完成Banner场景的加载

---
+++ Assets/Scripts/ConfigInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ConfigInfo
+{
+    public int bannerCount;
+    public string[] bannerSpritePaths;
+
+    private static ConfigInfo _instance;
+    private Sprite[] bannerSprites;
+
+    public static ConfigInfo Instance
+    {
+        get { return _instance; }
+        set
+        {
+            _instance = value;
+            _instance.UpdateSprite();
+        }
+    }
+
+    private void UpdateSprite()
+    {
+        bannerSprites = new Sprite[_instance.bannerCount];
+        for (int i = 0; i < bannerCount; i++)
+        {
+            Texture2D texture2D = Resources.Load<Texture2D>(bannerSpritePaths[i]);
+            bannerSprites[i] = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height),
+                Vector2.zero);
+        }
+    }
+
+    public Sprite getBannerSprite(int i)
+    {
+        if (i < 0 || i >= bannerCount)
+        {
+            Debug.Log("图片不存在");
+            return bannerSprites[0];
+        }
+        else
+        {
+            return bannerSprites[i];
+        }
+    }
+}
---
+++ Assets/Scripts/ConfigManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigManager : MonoBehaviour
+{
+    public Image bannerImage;
+
+    private void Awake()
+    {
+        var str = Resources.Load<TextAsset>("config").text;
+        print(str);
+        var configInfo = JsonUtility.FromJson<ConfigInfo>(str);
+        print(configInfo.bannerCount);
+        ConfigInfo.Instance = configInfo;
+    }
+
+    public void SetBannerImage(int i)
+    {
+        bannerImage.sprite = ConfigInfo.Instance.getBannerSprite(1);
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
